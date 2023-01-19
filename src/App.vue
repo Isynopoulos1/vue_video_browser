9abe0df4a8e45fd6bdb0c102247a0a2fb482c222
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-19 17:24:02 +0100

    updating the data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length }}
   </div>
 </template>
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 const API_KEY = "AIzaSyAtbDG8wXzKSnYVn6qpunmcTiASOmGInNY";
 
 export default {
   name: "App",
   components: {
     SearchBar,
+    VideoList,
   },
+  data() {
+    return { videos: [] };
+  },
+
   methods: {
     onTermChange(searchTerm) {
       axios
-        .get("https://googleapis.com/youtube/v3/search", {
+        .get("https://www.googleapis.com/youtube/v3/search", {
           params: {
             key: API_KEY,
             type: "video",
@@ -24,7 +32,9 @@ export default {
             q: searchTerm,
           },
         })
-        .then((response) => console.log(response));
+        .then((response) => {
+          this.videos = response.data.items;
+        });
     },
   },
 };
