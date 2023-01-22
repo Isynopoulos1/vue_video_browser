8d8dcf8e1393c05002458d6f8736fd48d13a8dc2
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-22 17:50:34 +0100

    two column layout

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="video">
-    <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" :src="videoUrl" />
+  <div v-if="video" class="col-md-8">
+    <div class="ratio ratio-16x9">
+      <iframe :src="videoUrl" />
     </div>
     <div class="details">
       <h4>{{ video.snippet.title }}</h4>
